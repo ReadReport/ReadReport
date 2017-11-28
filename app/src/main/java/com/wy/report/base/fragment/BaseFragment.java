3afc5dbfea254226d9e6d7a912a42db79284f468
linzhiwei <linzhiwei1@4399inc.com>
2017-11-28 12:26:06 +0800

    1. 增加失败重试功能

--- app/src/main/java/com/wy/report/base/fragment/BaseFragment.java
+++ app/src/main/java/com/wy/report/base/fragment/BaseFragment.java
@@ -2,7 +2,6 @@ package com.wy.report.base.fragment;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.support.annotation.CallSuper;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.Toolbar;
@@ -12,21 +11,9 @@ import android.view.ViewGroup;
 
 import com.hwangjr.rxbus.Bus;
 import com.hwangjr.rxbus.RxBus;
-import com.umeng.analytics.MobclickAgent;
 import com.wy.report.base.activity.BaseActivity;
 
 /*
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed To in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
  *
  * @author cantalou
  * @date 2017-11-24 22:53
