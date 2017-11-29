59bce3ea0e7b66128fc60474dc11b7ca03f508c1
zang_song <zang_song@gowild.cn>
2017-11-29 20:28:26 +0800

    1.添加报告详细

---
+++ app/src/main/java/com/wy/report/business/read/ReadReportActivity.java
@@ -0,0 +1,26 @@
+package com.wy.report.business.read;
+
+import com.wy.report.base.activity.BaseActivity;
+
+/**
+ * Author:      ZangSong
+ * Email:       gnoszsong@gmail.com
+ * Date:        17-11-29 下午8:18
+ * Description: ReadReport
+ */
+public class ReadReportActivity extends BaseActivity {
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected int contentLayoutID() {
+        return 0;
+    }
+}
