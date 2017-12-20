b29b75beee42e62409f575ca55966a9610b64b2a
linzhiwei <linzhiwei1@4399inc.com>
2017-12-20 13:35:42 +0800

    1. 上传功能开发

--- app/src/main/java/com/wy/report/business/upload/fragment/NotChainUnitFragment.java
+++ app/src/main/java/com/wy/report/business/upload/fragment/NotChainUnitFragment.java
@@ -1,13 +1,25 @@
 package com.wy.report.business.upload.fragment;
 
 import android.os.Bundle;
-import android.support.v4.view.ViewPager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
+import com.chad.library.adapter.base.BaseQuickAdapter;
+import com.chad.library.adapter.base.BaseViewHolder;
 import com.wy.report.R;
+import com.wy.report.base.constant.RxKey;
 import com.wy.report.base.fragment.PtrFragment;
+import com.wy.report.base.model.ResponseModel;
+import com.wy.report.business.upload.model.HospitalCityModel;
+import com.wy.report.business.upload.model.HospitalProvinceModel;
+import com.wy.report.business.upload.model.UnitModel;
+import com.wy.report.business.upload.service.HospitalService;
+import com.wy.report.helper.retrofit.RetrofitHelper;
+import com.wy.report.helper.retrofit.subscriber.PtrSubscriber;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 
@@ -25,16 +37,87 @@ public class NotChainUnitFragment extends PtrFragment {
     @BindView(R.id.recycle_view_right)
     RecyclerView recycleViewRight;
 
+    private BaseQuickAdapter<HospitalProvinceModel, BaseViewHolder> adapterLeft;
+
+    private BaseQuickAdapter<UnitModel, BaseViewHolder> adapterRight;
+
+    private HospitalService hospitalService;
+
+    private int selectedProvince = 0;
+
     @Override
     protected void initData(Bundle savedInstanceState) {
-
+        hospitalService = RetrofitHelper.getInstance()
+                                        .create(HospitalService.class);
     }
+
 
     @Override
     protected void initView(View contentView) {
         super.initView(contentView);
+
+        adapterLeft = new BaseQuickAdapter<HospitalProvinceModel, BaseViewHolder>(R.layout.vh_hospital_province) {
+            @Override
+            protected void convert(BaseViewHolder helper, HospitalProvinceModel item) {
+                helper.setText(R.id.vh_hospital_province, item.getProvince());
+            }
+        };
+        adapterLeft.setOnItemClickListener(new BaseQuickAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(BaseQuickAdapter adapter, View view, int position) {
+                showProvinceUnitData(position);
+            }
+        });
         recycleViewLeft.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false));
+        recycleViewLeft.setAdapter(adapterLeft);
+
+        adapterRight = new BaseQuickAdapter<UnitModel, BaseViewHolder>(R.layout.vh_hospital_province) {
+            @Override
+            protected void convert(BaseViewHolder helper, UnitModel item) {
+                helper.setText(R.id.vh_hospital_title, item.getProvince());
+            }
+        };
+        adapterRight.setOnItemClickListener(new BaseQuickAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(BaseQuickAdapter adapter, View view, int position) {
+                rxBus.post(RxKey.RX_HOSPITAL_UNIT_SELECT, adapterRight.getItem(position));
+                getActivity().finish();
+            }
+        });
         recycleViewRight.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false));
+        recycleViewRight.setAdapter(adapterRight);
+    }
+
+
+    @Override
+    protected void loadData() {
+        hospitalService.getNotChainUnits()
+                       .subscribe(new PtrSubscriber<ResponseModel<List<HospitalProvinceModel>>>(this) {
+                           @Override
+                           public void onNext(ResponseModel<List<HospitalProvinceModel>> listResponseModel) {
+                               super.onNext(listResponseModel);
+                               adapterLeft.setNewData(listResponseModel.getData());
+                           }
+                       });
+    }
+
+    private void showProvinceUnitData(int position) {
+
+        HospitalProvinceModel provinceModel = adapterLeft.getItem(selectedProvince);
+        if (provinceModel != null) {
+            provinceModel.setSelected(false);
+            adapterLeft.notifyItemChanged(selectedProvince);
+        }
+
+        provinceModel = adapterLeft.getItem(position);
+        provinceModel.setSelected(true);
+        adapterLeft.notifyItemChanged(position);
+        List<UnitModel> allUnits = new ArrayList<>();
+        for (HospitalCityModel cityModel : provinceModel.getCity()) {
+            allUnits.addAll(cityModel.getUnits());
+        }
+        adapterRight.setNewData(allUnits);
+        selectedProvince = position;
     }
 
     @Override
