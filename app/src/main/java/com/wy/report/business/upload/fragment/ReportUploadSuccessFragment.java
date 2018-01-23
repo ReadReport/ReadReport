f3942198c712e857eb354f9bd34e89b9a48e48ef
linzhiwei <linzhiwei1@4399inc.com>
2018-01-23 18:43:32 +0800

    1. 每日监测功能开发

---
+++ app/src/main/java/com/wy/report/business/upload/fragment/ReportUploadSuccessFragment.java
@@ -0,0 +1,23 @@
+package com.wy.report.business.upload.fragment;
+
+import android.os.Bundle;
+
+import com.wy.report.base.fragment.ToolbarFragment;
+
+/**
+ * @author cantalou
+ * @date 2018年01月23日 18:37
+ * <p>
+ */
+public class ReportUploadSuccessFragment extends ToolbarFragment {
+
+    @Override
+    protected int contentLayoutID() {
+        return 0;
+    }
+
+    @Override
+    protected void initToolbar() {
+        super.initToolbar();
+    }
+}
