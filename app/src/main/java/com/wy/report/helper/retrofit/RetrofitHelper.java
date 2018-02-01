ad82b2503a4eda4bb0bc4a74887c7bcf25b38233
linzhiwei <linzhiwei1@4399inc.com>
2018-02-01 15:26:59 +0800

    1. 增加日常检测数据为空情况

--- app/src/main/java/com/wy/report/helper/retrofit/RetrofitHelper.java
+++ app/src/main/java/com/wy/report/helper/retrofit/RetrofitHelper.java
@@ -5,12 +5,18 @@ import com.wy.report.helper.retrofit.converter.json.FastJsonConverterFactory;
 import com.wy.report.helper.retrofit.converter.part.PartConverterFactory;
 import com.wy.report.helper.retrofit.interceptor.AuthInterceptor;
 
+import org.apache.http.conn.ssl.AllowAllHostnameVerifier;
+
 import java.io.File;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 import java.util.HashMap;
 import java.util.concurrent.TimeUnit;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 import okhttp3.Cache;
 import okhttp3.OkHttpClient;
@@ -33,22 +39,52 @@ public class RetrofitHelper {
 
     private RetrofitHelper() {
 
-        ReportApplication context = ReportApplication.getGlobalContext();
-        File httpCacheDirectory = new File(context.getCacheDir(), "responses");
-        Cache cache = new Cache(httpCacheDirectory, 20 * 1024 * 1024);
+        try {
 
-        okHttpClient = new OkHttpClient().newBuilder()
-                                         .connectTimeout(30, TimeUnit.SECONDS)
-                                         .addNetworkInterceptor(new AuthInterceptor())
-                                         .cache(cache)
-                                         .build();
+            ReportApplication context = ReportApplication.getGlobalContext();
+            File httpCacheDirectory = new File(context.getCacheDir(), "responses");
+            Cache cache = new Cache(httpCacheDirectory, 20 * 1024 * 1024);
 
-        retrofit = new Retrofit.Builder().baseUrl("http://api.vip120.com")
-                                         .client(okHttpClient)
-                                         .addCallAdapterFactory(RxJavaCallAdapterFactory.createAsync())
-                                         .addConverterFactory(FastJsonConverterFactory.create())
-                                         .addConverterFactory(PartConverterFactory.create())
-                                         .build();
+            TrustManager[] trustAllCerts = new TrustManager[]{new X509TrustManager() {
+                @Override
+                public void checkClientTrusted(
+                        java.security.cert.X509Certificate[] x509Certificates,
+                        String s) throws java.security.cert.CertificateException {
+                }
+
+                @Override
+                public void checkServerTrusted(
+                        java.security.cert.X509Certificate[] x509Certificates,
+                        String s) throws java.security.cert.CertificateException {
+                }
+
+                @Override
+                public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                    return new java.security.cert.X509Certificate[]{};
+                }
+            }};
+
+            SSLContext sc = SSLContext.getInstance("TLS");
+            sc.init(null, trustAllCerts, new java.security.SecureRandom());
+
+            okHttpClient = new OkHttpClient().newBuilder()
+                                             .connectTimeout(30, TimeUnit.SECONDS)
+                                             .addNetworkInterceptor(new AuthInterceptor())
+                                             .cache(cache)
+                                             .hostnameVerifier(new AllowAllHostnameVerifier())
+                                             .sslSocketFactory(sc.getSocketFactory())
+                                             .build();
+
+            retrofit = new Retrofit.Builder().baseUrl("http://api.vip120.com")
+                                             .client(okHttpClient)
+                                             .addCallAdapterFactory(RxJavaCallAdapterFactory.createAsync())
+                                             .addConverterFactory(FastJsonConverterFactory.create())
+                                             .addConverterFactory(PartConverterFactory.create())
+                                             .build();
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public static RetrofitHelper getInstance() {
